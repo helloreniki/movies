14c65284b846f13ff87c0aa07862229f41817a30
Reniki <hello@reniki.com>
2023-11-16 18:52:24 +0100

    basic structure: route, HomeView, App

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <h1 class="text-2xl text-red-500">Vue</h1>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <header class="bg-gray-100 shadow-md">
+    <nav class="max-w-7xl mx-auto px-4 py-4 border-b">
+      <RouterLink :to="{ name: 'home' }" class="cursor-pointer">Home</RouterLink>
+    </nav>
+    <div class="max-w-7xl mx-auto px-4 py-20">
+      <h1 class="text-7xl font-semibold">Movies</h1>
     </div>
   </header>
 
-  <RouterView />
+  <div class="max-w-7xl mx-auto px-4 pt-16">
+    <RouterView />
+  </div>
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
