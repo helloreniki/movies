45afbe8bb5b3a99772ca582d3c91985e0ddfa54b
Reniki <hello@reniki.com>
2023-11-16 18:40:56 +0100

    tailwind install

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
+      <h1 class="text-2xl text-red-500">Vue</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
