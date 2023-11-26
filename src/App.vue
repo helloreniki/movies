69caf3f414096dd7b205df9874520ca520c5b177
Reniki <hello@reniki.com>
2023-11-26 11:58:52 +0100

    active link, preflight false styles

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+
+// const route = useRoute()
+// console.log('r',route.path)
 </script>
 
 <template>
-  <header class="bg-gray-100 shadow-md">
-    <nav class="max-w-7xl mx-auto px-4 py-4 border-b">
-      <RouterLink :to="{ name: 'home' }" class="cursor-pointer">Home</RouterLink>
+  <header class="bg-gradient-to-b from-gray-100 from-60% to-teal-100">
+    <nav class="max-w-7xl mx-auto px-4 py-4 flex gap-12 items-center text-xl">
+      <RouterLink :to="{ name: 'home' }" class="cursor-pointer text-gray-900 hover:text-teal-500 no-underline" active-class="text-teal-500">Home</RouterLink>
+      <RouterLink :to="{ name: 'my-lists' }" class="cursor-pointer text-gray-900 hover:text-teal-500 no-underline" active-class="text-teal-500">My Lists</RouterLink>
     </nav>
     <div class="max-w-7xl mx-auto px-4 py-20">
       <h1 class="text-7xl font-semibold">Movies</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
   ],
   // tailwind + primevue: Preflight is a set of base styles that Tailwind applies,
   // and it is setting the background color to transparent.
+  // preflight default styles: border-solid, bg-transparent, m-0 on body,...do this manually since preflight = false to use PrimeVue css
+  // documentation
   corePlugins: {
     preflight: false,
   },
